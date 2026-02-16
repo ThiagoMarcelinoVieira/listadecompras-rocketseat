033738c3e07559da8035947d5af83f67d6eee138
Thiago <thiagomarcelino897@gmail.com>
2026-02-16 19:41:38 -0300

    criando o localStorage

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ const inputAdd = document.getElementById("input-item")
 const form = document.getElementById("add-form")
 const list = document.querySelector("ul")
 const alertBox = document.getElementById("alert-box")
+
+// Atualiza o LocalStorage com os textos dos itens presentes no HTML
+function saveToStorage() {
+  const items = [];
+  document.querySelectorAll(".item-text").forEach(span => {
+    items.push(span.innerText);
+  });
+  localStorage.setItem("@quicklist:items", JSON.stringify(items));
+}
