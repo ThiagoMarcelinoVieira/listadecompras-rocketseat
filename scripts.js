f0f68fab33e39b424ec74f40426bbd35a9b7d424
Thiago <thiagomarcelino897@gmail.com>
2026-02-16 19:41:18 -0300

    iniciando o js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const inputAdd = document.getElementById("input-item")
+const form = document.getElementById("add-form")
+const list = document.querySelector("ul")
+const alertBox = document.getElementById("alert-box")
